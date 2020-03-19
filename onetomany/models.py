3b4baae3876b73f6a98ccb5824b242276bc7c84b
Victor Vess <victor.vess@vndly.com>
2020-03-18 20:56:42 -0400

    Update for Django 2 through field syntax

--- onetomany/models.py
+++ onetomany/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 # Create your models here.
 class OneToManyField(models.ManyToManyField):
-    """A forgein key field that behaves just like djangos ManyToMany field,
+    """A foreign key field that behaves just like Django's ManyToMany field,
     the only difference is that an instance of the other side can only be 
     related to one instance of your side. Also see the test cases.
     """
+
     def contribute_to_class(self, cls, name):
         # Check if the intermediate model will be auto created.
         # The intermediate m2m model is not auto created if:
@@ -13,12 +14,18 @@ class OneToManyField(models.ManyToManyField):
         #  2) The class owning the m2m field is abstract.
         #  3) The class owning the m2m field has been swapped out.
         auto_intermediate = False
-        if not self.rel.through and not cls._meta.abstract and not cls._meta.swapped:
+        if (
+            not self.remote_field.through
+            and not cls._meta.abstract
+            and not cls._meta.swapped
+        ):
             auto_intermediate = True
-        
-        #One call super contribute_to_class and have django create the intermediate model.
+
+        # One call super contribute_to_class and have django create the intermediate model.
         super(OneToManyField, self).contribute_to_class(cls, name)
 
         if auto_intermediate == True:
-            #Set unique_together to the 'to' relationship, this ensures a OneToMany relationship.
-            self.rel.through._meta.unique_together = ((self.rel.through._meta.unique_together[0][1],),)
+            # Set unique_together to the 'to' relationship, this ensures a OneToMany relationship.
+            self.remote_field.through._meta.unique_together = (
+                (self.remote_field.through._meta.unique_together[0][1],),
+            )
